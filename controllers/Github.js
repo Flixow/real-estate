91d878ec521dc09f5f8ba827e6201e65411fcc97
Daniel Siwek <danielsiwek95@gmail.com>
2017-08-14 23:05:37 +0200

    Change paths to the project

--- controllers/Github.js
+++ controllers/Github.js
@@ -2,7 +2,7 @@ const exec = require('child_process').exec
 
 exports.payload = (req, res) => {
   //verify that the payload is a push from the correct repo
-	//verify repository.name == 'wackcoon-device' or repository.full_name = 'DanielEgan/wackcoon-device'
+	//verify repository.name == 'real-estate' or repository.full_name = 'Flixow/real-estate'
 	console.log(req.body.pusher.name + ' just pushed to ' + req.body.repository.name)
 
 	console.log('pulling code from GitHub...')
@@ -12,16 +12,16 @@ exports.payload = (req, res) => {
   }
 
 	// reset any changes that have been made locally
-	exec('git -C ~/projects/wackcoon-device reset --hard', execCallback)
+	exec('git -C ~/Public/real-estate reset --hard', execCallback)
 
 	// and ditch any files that have been added locally too
-	exec('git -C ~/projects/wackcoon-device clean -df', execCallback)
+	exec('git -C ~/Public/real-estate clean -df', execCallback)
 
 	// now pull down the latest
-	exec('git -C ~/projects/wackcoon-device pull -f', execCallback)
+	exec('git -C ~/Public/real-estate pull -f', execCallback)
 
 	// and npm install with --production
-	exec('npm -C ~/projects/wackcoon-device install --production', execCallback)
+	exec('npm -C ~/Public/real-estate install --production', execCallback)
 
 	// and run tsc
 	exec('tsc', execCallback)
